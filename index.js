2cadb39e3b77d67786bd08535ac309a5d98d98ca
SoluyanovAndrey <a--b--s@yandex.ru>
2024-06-12 10:19:27 +0300

    menu

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import "./index.scss"
 //import "images/big.jpg"
-// import "./zeroing.scss"
-let b = 10;
-console.log (b);
+import "./zeroing.scss"
+
+function  toggleClass(id, toggleClass)
+    {
+        let p = document.getElementById(id);
+
+        p.addEventListener( "click" , () =>  p.classList.toggle(toggleClass));
+
+    }
+
+toggleClass("nav-icon1","open");
